3a95073d84ed9e51fed2481cbf81a39eedf5d391
eliaspucheta <eliaspucheta11@gmail.com>
2025-03-24 03:18:58 -0300

    admin added

--- lib/admin.ts
+++ lib/admin.ts
@@ -2,6 +2,7 @@ import { auth } from "@clerk/nextjs/server";
 
 const adminIds = [
   "user_2txZrU0MyMvUJzB0ARvgL37AWjF",
+  "user_2ukciIXPzndQKU9n6t5hv300pOm"
 ];
 
 export const getIsAdmin = async () => {
