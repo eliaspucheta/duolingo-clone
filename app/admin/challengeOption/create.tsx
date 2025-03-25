3cbeb81752fcd98a5a44dfe52b969939d9f7734f
eliaspucheta <eliaspucheta11@gmail.com>
2025-03-25 17:43:53 -0300

    word translation

--- app/admin/challengeOption/create.tsx
+++ app/admin/challengeOption/create.tsx
@@ -1,30 +1,24 @@
-import { SimpleForm, Create, BooleanInput, TextInput, required, ReferenceInput } from "react-admin";
+import {
+  SimpleForm,
+  Create,
+  BooleanInput,
+  TextInput,
+  required,
+  ReferenceInput,
+  AutocompleteInput,
+} from "react-admin";
 
 export const ChallengeOptionCreate = () => {
   return (
     <Create>
       <SimpleForm>
-        <TextInput
-          source="text"
-          validate={[required()]}
-          label="Texto"
-        />
-        <BooleanInput
-          source="correct"
-          label="Opción correcta"
-        />
-        <ReferenceInput
-          source="challengeId"
-          reference="Desafíos"
-        />
-        <TextInput
-          source="ImageSrc"
-          label="URL de la imagen"
-        />
-        <TextInput
-          source="audioSrc"
-          label="URL del audio"
-        />
+        <TextInput source="text" validate={[required()]} label="Texto" />
+        <BooleanInput source="correct" label="Opción correcta" />
+        <ReferenceInput source="challengeId" reference="challenges">
+          <AutocompleteInput label="Desafios" />
+        </ReferenceInput>
+        <TextInput source="ImageSrc" label="URL de la imagen" />
+        <TextInput source="audioSrc" label="URL del audio" />
       </SimpleForm>
     </Create>
   );
